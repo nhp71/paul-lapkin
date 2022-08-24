48135211ad1f22f894dc6f34e42b16fba6794a2c
nhpahel <nhpgithub@gmail.com>
2022-08-24 22:35:01 +0600

    first commit

---
+++ resources/js/main.js
@@ -0,0 +1,11 @@
+//menu
+
+function openMenu(){
+    document.getElementById('menu-container').style.width = "100%";
+};
+function closeMenu(){
+    document.getElementById('menu-container').style.width = "0%";
+};
+
+// mix it up 
+var mixer = mixitup('.work-grid');
